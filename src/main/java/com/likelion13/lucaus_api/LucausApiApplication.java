fc582823d49556d71e810f1a2861f05fff6a4284
eunsu02 <regulus_leo08@naver.com>
2025-03-15 13:31:23 +0900

    [setting] Initial commit

---
+++ src/main/java/com/likelion13/lucaus_api/LucausApiApplication.java
@@ -0,0 +1,13 @@
+package com.likelion13.lucaus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucausApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucausApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion13/lucaus_api/LucausApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion13.lucaus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucausApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
