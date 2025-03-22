dcdaf534591a4b193641925e1274f83f58535525
eunsu02 <regulus_leo08@naver.com>
2025-03-23 00:45:12 +0900

    [feat] 분실물 조회 기능 구현

---
+++ src/main/java/com/likelion13/lucaus_api/domain/repository/LostItemsRepository.java
@@ -0,0 +1,14 @@
+package com.likelion13.lucaus_api.domain.repository;
+
+import com.likelion13.lucaus_api.domain.entity.LostItems;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
+import com.likelion13.lucaus_api.domain.entity.LostItems.Category;
+
+public interface LostItemsRepository extends JpaRepository<LostItems, Long> {
+
+    Page<LostItems> findByCategoryAndUpdatedDateTimeBetween(
+            Category category, LocalDateTime startOfDay, LocalDateTime endOfDay, Pageable pageable);
+}
