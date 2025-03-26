0628637cbb1fe6f29aa6b492e19a7a08a0f78db4
eunsu02 <regulus_leo08@naver.com>
2025-03-27 00:36:55 +0900

    [feat] notionAPI json 파싱 및 저장을 위한 기본 설계

--- src/main/java/com/likelion13/lucaus_api/domain/repository/DetailedNoticesRepository.java
+++ src/main/java/com/likelion13/lucaus_api/domain/repository/DetailedNoticesRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DetailedNoticesRepository extends JpaRepository<DetailedNotices, Long> {
     Page<DetailedNotices> findAll(Pageable pageable);
     boolean existsByTitle(String title);
+    Optional<DetailedNotices> findByNotionId(String notionId);
 }
