4d634dbd15113de9709e337ebfebd390160336f3
eunsu02 <regulus_leo08@naver.com>
2025-03-27 11:56:50 +0900

    [fix] 스웨거 상에서 원치 않는 api가 여러개 생성되지 않도록 하는 코드

--- src/main/java/com/likelion13/lucaus_api/domain/repository/DetailedNoticesRepository.java
+++ src/main/java/com/likelion13/lucaus_api/domain/repository/DetailedNoticesRepository.java
@@ -5,9 +5,11 @@ import com.likelion13.lucaus_api.domain.entity.DetailedNotices;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface DetailedNoticesRepository extends JpaRepository<DetailedNotices, Long> {
     Page<DetailedNotices> findAll(Pageable pageable);
     boolean existsByTitle(String title);
--- src/main/java/com/likelion13/lucaus_api/domain/repository/ShortNoticesRepository.java
+++ src/main/java/com/likelion13/lucaus_api/domain/repository/ShortNoticesRepository.java
@@ -1,10 +1,12 @@
 package com.likelion13.lucaus_api.domain.repository;
 import com.likelion13.lucaus_api.domain.entity.ShortNotices;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface ShortNoticesRepository extends JpaRepository<ShortNotices, Long> {
     List<ShortNotices> findByIsVisibleTrue();
     Optional<ShortNotices> findByNotionId(String notionId);
