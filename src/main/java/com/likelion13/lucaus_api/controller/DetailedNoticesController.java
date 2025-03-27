ee7b1e639231ac14010176e25048b113f79cd649
eunsu02 <regulus_leo08@naver.com>
2025-03-27 11:59:44 +0900

    [feat] 공지사항 예외/에러 코드 추가 및 스웨거 설정

--- src/main/java/com/likelion13/lucaus_api/controller/DetailedNoticesController.java
+++ src/main/java/com/likelion13/lucaus_api/controller/DetailedNoticesController.java
@@ -3,12 +3,18 @@ package com.likelion13.lucaus_api.controller;
 import com.likelion13.lucaus_api.dto.response.DetailedNoticesResponseDto;
 import com.likelion13.lucaus_api.service.DetailedNoticesService;
 import com.likelion13.lucaus_api.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/notices")
+@Tag(name = "Detailed Notices", description = "Operations related to detailed notices")
 public class DetailedNoticesController {
 
     private final DetailedNoticesService detailedNoticesService;
@@ -19,6 +25,15 @@ public class DetailedNoticesController {
     }
 
     @GetMapping
+    @Operation(summary = "공지사항", description = "공지사항을 조회하는 API입니다. ")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적 조회",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class)))
+    })
     public ApiResponse<Page<DetailedNoticesResponseDto>> getNotices(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size) {
