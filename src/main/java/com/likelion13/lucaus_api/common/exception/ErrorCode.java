9b3531e450f1165fe9335be9557cb9782790e9c5
eunsu02 <regulus_leo08@naver.com>
2025-03-27 11:56:18 +0900

    [feat] 분실물 API 스웨거 설정

--- src/main/java/com/likelion13/lucaus_api/common/exception/ErrorCode.java
+++ src/main/java/com/likelion13/lucaus_api/common/exception/ErrorCode.java
@@ -11,7 +11,12 @@ public enum ErrorCode implements BaseErrorCode {
     // 일반적인 응답
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "CATEGORY400", "잘못된 카테고리 입니다. 유효한 카테고리 (COSMETICS, ELECTRONICS, CLOTHING, WALLET_CARD, OTHERS, TOTAL)"),
+    INVALID_DATE(HttpStatus.BAD_REQUEST, "DATE400", "잘못된 날짜 형식입니다. (형식: YYYY-MM-DD)"),
+    INVALID_PAGE_SIZE(HttpStatus.BAD_REQUEST, "PAGE400", "페이지와 사이즈는 1보다 커야합니다."),
+    INVALID_EMPTY(HttpStatus.BAD_REQUEST, "EMPTY400", "카테고리,날짜,페이지,사이즈는 필수값 입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/likelion13/lucaus_api/controller/LostItemsController.java
+++ src/main/java/com/likelion13/lucaus_api/controller/LostItemsController.java
@@ -1,7 +1,12 @@
 package com.likelion13.lucaus_api.controller;
 
+import com.likelion13.lucaus_api.common.exception.GeneralHandler;
 import com.likelion13.lucaus_api.dto.response.LostItemsResponseDto;
 import com.likelion13.lucaus_api.service.LostItemsService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +15,23 @@ import com.likelion13.lucaus_api.common.response.ApiResponse;
 
 @RestController
 @RequestMapping("/api/lost-items")
+@Tag(name = "LostItems", description = "Operations related to LostItems")
 public class LostItemsController {
 
     @Autowired
     private LostItemsService lostItemsService;
 
     @GetMapping
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적 조회",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청 - 잘못된 카테고리나 날짜",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청 - 잘못된 페이지 번호나 크기",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class)))
+    })
     public ApiResponse<Page<LostItemsResponseDto>> getLostItems(
             @RequestParam @NotNull String category,
             @RequestParam @NotNull String date,
@@ -24,12 +40,11 @@ public class LostItemsController {
 
         try {
             Page<LostItemsResponseDto> responseDtoPage = lostItemsService.getLostItems(category, date, page, size);
-
             return ApiResponse.onSuccess(responseDtoPage);
-
-        } catch (Exception e) {
-            return ApiResponse.onFailure("500", "Internal Server Error", null);
+        } catch (GeneralHandler e) {
+            return ApiResponse.onFailure(e.getCode().toString(), e.getMessage(), null);
         }
+
     }
 }
 
