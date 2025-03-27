66d8fc9884c62f3c5fdbb7208402668f6e9010e1
eunsu02 <regulus_leo08@naver.com>
2025-03-27 11:59:50 +0900

    [feat] 한줄안내 예외/에러 코드 추가 및 스웨거 설정

--- src/main/java/com/likelion13/lucaus_api/controller/ShortNoticesController.java
+++ src/main/java/com/likelion13/lucaus_api/controller/ShortNoticesController.java
@@ -3,6 +3,10 @@ package com.likelion13.lucaus_api.controller;
 import com.likelion13.lucaus_api.dto.response.ShortNoticesResponseDto;
 import com.likelion13.lucaus_api.service.ShortNoticesService;
 import com.likelion13.lucaus_api.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +16,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/short-notices")
+@Tag(name = "ShortNotices", description = "Operations related to ShortNotices")
 public class ShortNoticesController {
 
     @Autowired
     private ShortNoticesService shortNoticesService;
 
     @GetMapping
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적 조회",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class)))
+    })
     public ApiResponse<List<ShortNoticesResponseDto>> getVisibleShortNotices() {
-        try {
             List<ShortNoticesResponseDto> responseDtoList = shortNoticesService.getVisibleShortNotices();
-
             return ApiResponse.onSuccess(responseDtoList);
-        } catch (Exception e) {
-            return ApiResponse.onFailure("500", "Internal Server Error", null);
-        }
     }
 }
