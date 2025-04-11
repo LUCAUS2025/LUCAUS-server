05d0fe9f91a5feda643fb8abb68047667e317528
kimtree24 <ktj503@naver.com>
2025-04-11 23:20:54 +0900

    [feat] 푸드트럭 상세보기 api 구현

--- src/main/java/com/likelion13/lucaus_api/controller/FoodTruckController.java
+++ src/main/java/com/likelion13/lucaus_api/controller/FoodTruckController.java
@@ -1,7 +1,10 @@
 package com.likelion13.lucaus_api.controller;
 
 import com.likelion13.lucaus_api.common.response.ApiResponse;
+import com.likelion13.lucaus_api.dto.response.FoodTruck.FoodTruckDetailResponseDto;
 import com.likelion13.lucaus_api.dto.response.FoodTruck.FoodTruckListByDateResponseDto;
+import com.likelion13.lucaus_api.dto.response.booth.BoothDetailResponseDto;
+import com.likelion13.lucaus_api.service.foodTruck.FoodTruckDetailService;
 import com.likelion13.lucaus_api.service.foodTruck.FoodTruckOpDateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +21,17 @@ public class FoodTruckController {
 
     private final FoodTruckOpDateService foodTruckOpDateService;
 
+    private final FoodTruckDetailService foodTruckDetailService;
+
     @GetMapping("/{opDate}")
     public ApiResponse<List<FoodTruckListByDateResponseDto>> getFoodTruckListByDate(@PathVariable Integer opDate){
         List<FoodTruckListByDateResponseDto> result = foodTruckOpDateService.getFoodTruckListByDate(opDate);
+        return ApiResponse.onSuccess(result);
+    }
+
+    @GetMapping("/{opDate}/{dayFoodTruckNum}")
+    public ApiResponse<List<FoodTruckDetailResponseDto>> getFoodTruckDetail(@PathVariable Integer opDate, @PathVariable Integer dayFoodTruckNum){
+        List<FoodTruckDetailResponseDto> result = foodTruckDetailService.getFoodTruckDetailByOpDateAndDayFoodTruckNum(opDate, dayFoodTruckNum);
         return ApiResponse.onSuccess(result);
     }
 }
---
+++ src/main/java/com/likelion13/lucaus_api/dto/response/FoodTruck/FoodTruckDetailResponseDto.java
@@ -0,0 +1,23 @@
+package com.likelion13.lucaus_api.dto.response.FoodTruck;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class FoodTruckDetailResponseDto {
+    private Integer dayFoodTruckNum; // 날짜별 푸드트럭 번호
+
+    private String name; // 푸드트럭 이름
+
+    private String cover; // 푸드트럭 커버 이미지
+
+    private String location;// 위치
+
+    private List<Map<String, Integer>> menus; // 메뉴들(메뉴이름, 가격)
+
+    private List<Map<String, Integer>> foodTruckReviews; // 푸드트럭리뷰
+}
