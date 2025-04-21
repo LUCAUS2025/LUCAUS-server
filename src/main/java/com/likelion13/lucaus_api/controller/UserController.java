bd91092f08ee3b136a44446dfdb4cd1f58ffa676
kimtree24 <ktj503@naver.com>
2025-04-21 22:54:23 +0900

    [feat] 도장판 관련 스웨거 문서정리 완료

--- src/main/java/com/likelion13/lucaus_api/controller/UserController.java
+++ src/main/java/com/likelion13/lucaus_api/controller/UserController.java
@@ -3,6 +3,11 @@ package com.likelion13.lucaus_api.controller;
 import com.likelion13.lucaus_api.common.response.ApiResponse;
 import com.likelion13.lucaus_api.dto.response.user.UserInfoResponseDto;
 import com.likelion13.lucaus_api.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "유저", description = "유저 관련 API입니다.(jwt필요)")
 public class UserController {
     private final UserService userService;
 
     @GetMapping
+    @Operation(summary = "유저 정보", description = "유저 정보 가져오기 API입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "성공적 조회",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON500", description = "서버 내부 오류",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "LOGIN404", description = "존재하지 않는 ID",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class))),
+    })
     public ApiResponse<UserInfoResponseDto> getUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
         String userId = userDetails.getUsername();
         UserInfoResponseDto result = userService.getCurrentUserInfo(userId);
