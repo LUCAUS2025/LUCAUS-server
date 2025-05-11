d1bf1d2282de2a24dbb6c466b34a51ebd51f9d3a
eunsu02 <regulus_leo08@naver.com>
2025-05-11 13:22:24 +0900

    [fix] 스케쥴러 가동

--- src/main/java/com/likelion13/lucaus_api/service/notion/NotionService.java
+++ src/main/java/com/likelion13/lucaus_api/service/notion/NotionService.java
@@ -381,7 +381,7 @@ public class NotionService {
 
     //     ShortNotice 데이터를 주기적으로 가져오는 메서드
 
-//    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void fetchShortNotices() {
         List<ShortNoticeDto> shortNotices = fetchNotionDataFromDatabaseId(notionConfig.getShortNoticesDbId(), ShortNoticeDto.class);
 
